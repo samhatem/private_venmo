39f56ca12180ecc1733d0234f42120b49c42391d
Sam Hatem <samhatem@Sams-Macbook-Pro.local>
2019-11-19 16:07:42 -0500

    got truffle working

--- contracts/Zdai.sol
+++ contracts/Zdai.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-contract Migrations {
+contract Zdai {
   address public owner;
   uint public last_completed_migration;
 
@@ -8,16 +8,4 @@ contract Migrations {
     owner = msg.sender;
   }
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
-
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
 }
--- test/Zdai_test.js
+++ test/Zdai_test.js
@@ -4,6 +4,10 @@ contract('Zdai test', async (accounts) => {
   let instance
 
   before(async () => {
-    instance = await Zdai.deployed();
-  });
+    instance = await Zdai.deployed()
+  })
+
+  it("The smart contract was deployed", async () => {
+    console.log('sc ready for calls')
+  })
 })
